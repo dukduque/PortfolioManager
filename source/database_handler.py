af7dd21e4911caf527a2248c7810ae1cfdd5565b
Daniel Duque <danielduque@u.northwestern.edu>
2019-05-23 23:43:00 -0500

    First commit

---
+++ source/database_handler.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 23 22:32:31 2019
+
+@author: dduque
+"""
+
+'''
+Setup paths
+'''
+import os 
+import sys
+path_to_file = os.path.dirname(os.path.realpath(__file__))
+parent_path = os.path.abspath(os.path.join(path_to_file, os.pardir))
+sys.path.append(parent_path)
+path_to_data = os.path.abspath(os.path.join(parent_path,  'data'))
+
+'''
+Setup libraries
+'''
+import pandas as pd
+import pandas_datareader.data as web
+import datetime
+import pickle
+
+
+def load_database(DB_file_name):
+    path_to_database = os.path.join(path_to_data,DB_file_name)
+    exists = os.path.isfile(path_to_database)
+    if exists == False:
+        return None
+    
+    try:
+        return pd.read_pickle(path_to_database)
+    except Exception as e: 
+        print(e)
+        
+def safe_database(DB_file_name, BD):
+    path_to_database = os.path.join(path_to_data,DB_file_name)
+    exists = os.path.isfile(path_to_database)
+    if exists:
+        #TODO: Make a copy of the existing file
+        pass
+    BD.to_pickle(path_to_database)
+    
+        
+
+
+F = web.DataReader('F', 'yahoo')
---
+++ source/opt_tools.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 23 22:35:42 2019
+
+@author: dduque
+"""
+
+
+def opt_model_1(a1,a2):
+    return a1+a2
