084b23ccb27adb02796ebefa9e57b266db024d4d
Daniel Duque <danielduque@u.northwestern.edu>
2019-05-23 23:49:47 -0500

    First files

---
+++ source/test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 23 22:36:26 2019
+
+@author: dduque
+"""
+
+
+from source.opt_tools import opt_model_1
+
+print(opt_model_1(1,2))
