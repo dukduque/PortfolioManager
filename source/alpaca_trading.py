0a77c681f29d1a7f11dc1176d64a622a9c6d09ae
Daniel Duque <danielduque@u.northwestern.edu>
2019-07-18 08:03:15 -0500

    Fixed filter of the database to avoid issues when the update is at midnight.

--- source/alpaca_trading.py
+++ source/alpaca_trading.py
@@ -5,55 +5,91 @@ Created on Sun Jul 14 12:53:19 2019
 
 @author: dduque
 """
-
 from os import environ
-
-environ['APCA_API_KEY_ID'] = 'PKVUICJDJKBJKSXJX6DS'
-environ['APCA_API_SECRET_KEY'] = 'xxskdLa9c8jYpUrZAf27uZUc3TNQ2UiV819zkLIa'
+from opt_tools import cvar_model_pulp
+import database_handler as dbh 
+import datetime as dt
+import pickle
+import alpaca_trade_api as tradeapi
+environ['APCA_API_KEY_ID'] = 'PKN0EHU7EQ2ML1IA0KRI'
+environ['APCA_API_SECRET_KEY'] = '/cmrcUS7eoD2MSWY5OZQAI6VEolDKEspevu5KIJd'
 environ['APCA_API_BASE_URL'] = 'https://paper-api.alpaca.markets/'
-import alpaca_trade_api as  tradeapi
-
-api = tradeapi.REST('PKX51YFNEGD687QL542B', '8BCNvLMgWbjafSMi2/dz8fpNh2O2l4iYjJkMm0k6')
 
 
-#orders = api.list_orders()
-#print()
-#for o in orders:
+#api = tradeapi.REST('PKN0EHU7EQ2ML1IA0KRI','/cmrcUS7eoD2MSWY5OZQAI6VEolDKEspevu5KIJd')
+api = tradeapi.REST()
+
+
+# orders = api.list_orders()
+# print(orders)
+# print()
+# for o in orders:
 #    print(o.symbol, o.status)
 #    #api.cancel_order(order_id=o.id)
-    
 
 # Submit a market order to buy 1 share of Apple at market price
-import pickle
-portfolios = pickle.load(open('./portfolios.p', 'rb'))
-portfolio = portfolios[2]
-for s in portfolio.index:
-    print(s, portfolio.loc[s,'stock'])
-    api.submit_order(
-        symbol=s,
-        qty=portfolio.loc[s,'stock'],
-        side='buy',
-        type='market',
-        time_in_force='gtc'
-    )
-print("loaded")
+
+# portfolios = pickle.load(open('./portfolios.p', 'rb'))
+# portfolio = portfolios[2]
+# for s in portfolio.index:
+#     print(s, portfolio.loc[s, 'stock'])
+#     api.submit_order(
+#         symbol=s,
+#         qty=portfolio.loc[s, 'stock'],
+#         side='buy',
+#         type='market',
+#         time_in_force='gtc'
+#     )
+# print("loaded")
 #
 
-#api.submit_order(
+# api.submit_order(
 #    symbol='GOOGL',
 #    qty=5,
 #    side='buy',
 #    type='market',
 #    time_in_force='gtc'
-#)
+# )
 #
-## Submit a limit order to attempt to sell 1 share of AMD at a
-## particular price ($20.50) when the market opens
-#api.submit_order(
+# Submit a limit order to attempt to sell 1 share of AMD at a
+# particular price ($20.50) when the market opens
+# api.submit_order(
 #    symbol='AMD',
 #    qty=1,
 #    side='buy',
 #    type='limit',
 #    time_in_force='opg',
 #    limit_price=20.50
-#)
+# )
+
+
+def rebalance(api, additions=0, stock_univers='sp500'):
+    '''
+    Args:
+        api (Object): trading api.
+        additions (float): additions in capital.
+        stock_universe (str): set of stocks to consider.
+            available options are sp500 (default) and rusell1000.
+    '''
+
+    # Get current portfolio
+    current_portfolio = api.list_positions()
+    mkt_val = sum(float(position.market_value) for position in current_portfolio)
+    new_mkt_val = mkt_val + additions
+
+    today = dt.datetime.today()
+    my_date = today.replace(today.year - 2)  
+    stocks = dbh.save_sp500_tickers() if stock_univers == 'sp500' else dbh.save_rusell1000_tickers()
+    db, data = dbh.get_returns(
+        data_file='close.pkl', start_date=my_date,
+        stocks=stocks)
+    '''
+    Create modelwith default parameters
+    '''
+    price = db.iloc[-1]  # Last row is the current price
+    opt_model = cvar_model_pulp(data, price, budget=new_mkt_val, fractional=True)
+
+if __name__ == '__main__':
+    api = tradeapi.REST()
+    #dbh.run_update_process()
+    rebalance(api)
--- source/database_handler.py
+++ source/database_handler.py
@@ -13,50 +13,53 @@ data is yfinance.
 '''
 Setup paths
 '''
-import os 
+
+
+import shutil
+from alpha_vantage.timeseries import TimeSeries  # API limits, 5 queries per minute
+import pandas_datareader.data as web  # yahoo or av-daily, but glichy
+import yfinance as yf  # Works awsome!
+import requests
+import pickle
+import datetime
+import itertools
+import bs4 as bs
+import multiprocessing as mp
+import numpy as np
+import pandas as pd
 import sys
+import os
 path_to_file = os.path.dirname(os.path.realpath(__file__))
 parent_path = os.path.abspath(os.path.join(path_to_file, os.pardir))
 sys.path.append(parent_path)
-path_to_data = os.path.abspath(os.path.join(parent_path,  'data'))
-path_to_output = os.path.abspath(os.path.join(parent_path,  'output'))
+path_to_data = os.path.abspath(os.path.join(parent_path, 'data'))
+path_to_output = os.path.abspath(os.path.join(parent_path, 'output'))
 
+import source.popt_utils as popt_utils
 '''
 Setup libraries
 '''
-import source.popt_utils as popt_utils
-import pandas as pd
-import numpy as np
-import multiprocessing as mp
-import bs4 as bs
-import itertools
-import yfinance as yf #Works awsome!
-import pandas_datareader.data as web #yahoo or av-daily, but glichy
-from alpha_vantage.timeseries import TimeSeries #API limits, 5 queries per minute
 AV_TS = TimeSeries(key='OSZLY662JJE9SVS1', output_format='pandas')
-import datetime
-import pickle
-import shutil 
-import requests
-
 
 
 def save_sp500_tickers():
     '''
     https://pythonprogramming.net/sp500-company-list-python-programming-for-finance/
     '''
-    resp = requests.get('http://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+    resp = requests.get(
+        'http://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
     soup = bs.BeautifulSoup(resp.text, 'lxml')
     table = soup.find('table', {'class': 'wikitable sortable'})
     tickers = []
     for row in table.findAll('tr')[1:]:
         ticker = row.findAll('td')[0].text
-        tickers.append(ticker.replace('\n',''))
-    path_to_file = os.path.join(path_to_data,"sp500tickers.pickle")
-    with open(path_to_file,"wb") as f:
-        pickle.dump(tickers,f)
-        
+        tickers.append(ticker.replace('\n', ''))
+    path_to_file = os.path.join(path_to_data, "sp500tickers.pickle")
+    with open(path_to_file, "wb") as f:
+        pickle.dump(tickers, f)
+
     return tickers
+
 
 def save_rusell1000_tickers():
     '''
@@ -69,13 +72,12 @@ def save_rusell1000_tickers():
     tickers = []
     for row in table.findAll('tr')[1:]:
         ticker = row.findAll('td')[1].text
-        tickers.append(ticker.replace('\n',''))
-    path_to_file = os.path.join(path_to_data,"rusell1000tickers.pickle")
-    with open(path_to_file,"wb") as f:
-        pickle.dump(tickers,f)
-        
-    return tickers
+        tickers.append(ticker.replace('\n', ''))
+    path_to_file = os.path.join(path_to_data, "rusell1000tickers.pickle")
+    with open(path_to_file, "wb") as f:
+        pickle.dump(tickers, f)
 
+    return tickers
 
 
 def load_database(DB_file_name):
@@ -84,16 +86,17 @@ def load_database(DB_file_name):
     Args:
         DB_file_name (str): name of the file
     '''
-    path_to_database = os.path.join(path_to_data,DB_file_name)
+    path_to_database = os.path.join(path_to_data, DB_file_name)
     exists = os.path.isfile(path_to_database)
     if exists == False:
-        raise 'File %s does not exist' %(DB_file_name)
+        raise 'File %s does not exist' % (DB_file_name)
     try:
         return pd.read_pickle(path_to_database)
-    except Exception as e: 
+    except Exception as e:
         print(e)
-        
-def save_database(BD,DB_file_name):
+
+
+def save_database(BD, DB_file_name):
     '''
     Saves a database of in a pickle file. If a such file
     already exists, a copy of the old file is created.
@@ -101,17 +104,16 @@ def save_database(BD,DB_file_name):
         DB (DataFrame): a pandas data frame
         DB_file_name (str): name of the file
     '''
-    path_to_database = os.path.join(path_to_data,DB_file_name)
+    path_to_database = os.path.join(path_to_data, DB_file_name)
     exists = os.path.isfile(path_to_database)
     if exists:
-        copy_name = 'copy_%s' %(DB_file_name)
-        copy_path = os.path.join(path_to_data,copy_name)
-        shutil.copyfile(path_to_database, copy_path)  
+        copy_name = 'copy_%s' % (DB_file_name)
+        copy_path = os.path.join(path_to_data, copy_name)
+        shutil.copyfile(path_to_database, copy_path)
     BD.to_pickle(path_to_database)
 
 
-
-def create_database(stock_symbol='GOOGLE', start = None, end = None):
+def create_database(stock_symbol='GOOGLE', start=None, end=None):
     '''
     Creates a dataframe with one stock.
     Args:
@@ -123,36 +125,34 @@ def create_database(stock_symbol='GOOGLE', start = None, end = None):
         status (bool): true if the query was succesfull
     '''
     print(stock_symbol, start, end)
-    
+
     try:
-#        db = web.DataReader(stock_symbol, "av-daily", start=start ,end=end, access_key='OSZLY662JJE9SVS1')
-#        db['date'] =  [dt.datetime.strptime(d, '%Y-%m-%d') for d in db.index]
-#        db = db.set_index('date')
-#        db = db.close
-        
-#        db = web.DataReader(stock_symbol, 'yahoo', start=start, end=end)
-#        db = db.Close
-        
+        #        db = web.DataReader(stock_symbol, "av-daily", start=start ,end=end, access_key='OSZLY662JJE9SVS1')
+        #        db['date'] =  [dt.datetime.strptime(d, '%Y-%m-%d') for d in db.index]
+        #        db = db.set_index('date')
+        #        db = db.close
+
+        #        db = web.DataReader(stock_symbol, 'yahoo', start=start, end=end)
+        #        db = db.Close
+
         stock = yf.Ticker(stock_symbol)
-        db = stock.history(start = start, end = end)
+        db = stock.history(start=start, end=end)
         db = db.Close
-        
+
         db = db.loc[~db.index.duplicated(keep='last')]
-        db = db[db.index>=start]
+        db = db[db.index >= start]
         db.rename(stock_symbol, inplace=True)
         return stock_symbol, db, True
-    except Exception as e: 
+    except Exception as e:
         print(e)
         print('Fail to get: ', stock_symbol, start, end)
-    
+
     return stock_symbol, None, False
-         
-        
+
 
 def create_database_mp(input_date):
     return create_database(*input_date)
-    
-    
+
 
 def add_stock(db, stock_symbol, start=None, end=None):
     '''
@@ -162,10 +162,10 @@ def add_stock(db, stock_symbol, start=None, end=None):
     '''
     _, ndb, status = create_database(stock_symbol, start, end)
     if status:
-        return pd.concat((db,ndb),axis=1,join='outer'), True
+        return pd.concat((db, ndb), axis=1, join='outer'), True
     else:
         return db, False
-    
+
 
 def quotien_diff(x):
     '''
@@ -174,29 +174,31 @@ def quotien_diff(x):
     Args:
         x (DataSeries): pandas data series
     '''
-    return np.divide(x[1:],x[:-1])
- 
+    return np.divide(x[1:], x[:-1])
 
-def get_returns(data_file, start_date='2000', stocks = []):
+
+def get_returns(data_file, start_date='2000', stocks=[]):
     '''
     Computes the returns for stocks in the data file from
-    a given year. All prices should be avaialbe to consider 
+    a given year. All prices should be avaialbe to consider
     a stock.
     Args:
         data_file (str): database file
         start_date (datetime): initial date
     '''
-    assert start_date >= datetime.datetime(1970,1,1), 'Year should be from 1970'
+    assert start_date >= datetime.datetime(
+        1970, 1, 1), 'Year should be from 1970'
     db = load_database(data_file)
-    if len(stocks)>0:
+    if len(stocks) > 0:
         db = db[db.columns.intersection(stocks)]
-    db = db[db.index>start_date]
+    db = db[db.index > start_date]
+    db = db.dropna(axis=0, how='all')
     db = db.dropna(axis=1)
-    db_r = db.apply(quotien_diff,axis=0) #compute returns
-    db_r = db_r[db_r<10.0].dropna(axis=1) #Filter outliers
+    db_r = db.apply(quotien_diff, axis=0)  # compute returns
+    db_r = db_r[db_r < 10.0].dropna(axis=1)  # Filter outliers
     db = db.filter(db_r.columns, axis=1)
     db = db.filter(db_r.index, axis=0)
-    
+
     r_data = np.array(db_r)
     return db, r_data
 
@@ -205,97 +207,90 @@ def cov_estimation():
     pass
 
 
-
-
-
 def update_database(db, n_proc=1):
     '''
     Updates a database from the last prices.
     If n_proc > 1, runs a mutiprocess version of
     the function to speedup the colection of data.
     '''
-    ts = db.index[-1] #get last date in DB
-    ndb = pd.DataFrame() 
+    ts = db.index[-1]  # get last date in DB
+    ndb = pd.DataFrame()
     failed_stocks = []
-    print('Updating %i stock with %i processors' %(len(db.columns), n_proc))
+    print('Updating %i stock with %i processors' % (len(db.columns), n_proc))
     if n_proc > 1:
         data_pool = mp.Pool(n_proc)
         stock_list = db.columns.to_list()
         n = 100
-        chunks = [stock_list[i*n:(i+1)*n] for i in range((len(stock_list)+n-1)//n)]
+        chunks = [stock_list[i*n:(i+1)*n]
+                  for i in range((len(stock_list)+n-1)//n)]
         for chunk in chunks:
-            stock_tasks = itertools.product(chunk,[ts])
-            mp_out = data_pool.map(create_database_mp,stock_tasks)
+            stock_tasks = itertools.product(chunk, [ts])
+            mp_out = data_pool.map(create_database_mp, stock_tasks)
             for s, db_s, status_s in mp_out:
                 if status_s:
-                    ndb = pd.concat((ndb, db_s),axis=1,join='outer')   
+                    ndb = pd.concat((ndb, db_s), axis=1, join='outer')
                 else:
                     failed_stocks.append(s)
-                    
+
         data_pool.close()
     else:
         for c in db.columns:
             try:
                 ndb, status = add_stock(ndb, c, start=ts)
-                if status == False: #No updated was performed
+                if status == False:  # No updated was performed
                     failed_stocks.append(c)
-            except Exception as e: 
+            except Exception as e:
                 print(e)
-    print(failed_stocks)      
-    out_db = pd.concat((db,ndb),axis=0,join='outer')
+    print(failed_stocks)
+    out_db = pd.concat((db, ndb), axis=0, join='outer')
     out_db = out_db.loc[~out_db.index.duplicated(keep='last')]
     return out_db
-    
 
-        
- 
+
 def download_all_data(DB_file_name):
     symbols_df = web.get_nasdaq_symbols()
-    symbols_df = symbols_df[symbols_df.ETF==False]
+    symbols_df = symbols_df[symbols_df.ETF == False]
     sym_list = list(symbols_df.index)
-    
+
     db1, _ = create_database(sym_list[0], start=1900)
-    for i in range(1,len(sym_list)):
+    for i in range(1, len(sym_list)):
         try:
-            db1 = add_stock(db1,sym_list[i],start='1900')
-            if i % 100 == 0 :
+            db1 = add_stock(db1, sym_list[i], start='1900')
+            if i % 100 == 0:
                 cols = len(db1.columns)
-                print('Got %i stocks for far' %(cols))
-                save_database(db1,DB_file_name)
-        except Exception as e: 
+                print('Got %i stocks for far' % (cols))
+                save_database(db1, DB_file_name)
+        except Exception as e:
             print(e)
     return db1
 
-    
-    
-
+def run_update_process(db_file_in='close.pkl', db_file_out='close.pkl', n_proc=4):
+    db = load_database(db_file_in)
+    db = update_database(db, n_proc)
+    save_database(db, db_file_out)
 
 if __name__ == '__main__':
     args = popt_utils.dh_parse_arguments()
     print(args)
     if args.a == 'u':
         today_ts = datetime.datetime.today()
-        db = load_database(args.db_file)
-        db = update_database(db, args.n_proc)
-        
         str_today = str(today_ts)
-        out_file = 'close_%s.pkl' %(str_today.split(' ')[0])
-        save_database(db, out_file)
-        
-     
+        out_file = 'close_%s.pkl' % (str_today.split(' ')[0])
+        run_update_process(arg.db_file, out_file, arg.proc)
+
     #db = load_database('close_2019-05-26.pkl')
     #ini_time = datetime.datetime(2019,5,20)
     #new_db = yf.download(db.columns.to_list(), start = ini_time)
     #db = update_database(db, 20)
     #save_database(db, 'close_2019_05_26.pkl')
-    
-#for stock_symbol in db.columns[:10]:
+
+# for stock_symbol in db.columns[:10]:
 #    stock = yf.Ticker(stock_symbol)
 #    s_data = stock.history('5d')
 #    print(s_data.head())
-#    
-#    
-#if False:
+#
+#
+# if False:
 #
 #    # Get json object with the intraday data and another with  the call's metadata
 #    data, meta_data = ts.get_daily('GOOGL')
