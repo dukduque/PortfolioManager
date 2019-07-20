08dbad114facb4d9207c0ae2d93ad804df1b1084
Daniel Duque <danielduque@u.northwestern.edu>
2019-07-20 11:19:15 -0500

    Added rebalance function for alpaca.

--- source/popt_utils.py
+++ source/popt_utils.py
@@ -13,12 +13,13 @@ import os
 import argparse
 
 
-
 def dh_parse_arguments():
     parser = argparse.ArgumentParser("Datahandler parser")
-    parser.add_argument("-a", choices=['u'], default='u', help='Action to perform')
-    parser.add_argument("-db_file" ,  type=str, help='File name of the database to update')
-    parser.add_argument("-n_proc" , default=4, type=int, help='Number of processor to use')
+    parser.add_argument("-a", choices=['u'],
+                        default='u', help='Action to perform')
+    parser.add_argument("-db_file",  type=str,
+                        help='File name of the database to update')
+    parser.add_argument("-n_proc", default=4, type=int,
+                        help='Number of processor to use')
     args = parser.parse_args()
     return args
-
