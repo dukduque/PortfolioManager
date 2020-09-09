6101883514fb0f6363d22468ce3713decfdcac20
nicolasvd89 <nicolasvillarrealdaza@gmail.com>
2020-09-09 18:47:13 -0400

    Additional fixes to download stocks

--- source/popt_utils.py
+++ source/popt_utils.py
@@ -9,17 +9,13 @@ Created on Sun May 26 23:15:56 2019
 import sys
 import os
 
-
 import argparse
 
 
 def dh_parse_arguments():
     parser = argparse.ArgumentParser("Datahandler parser")
-    parser.add_argument("-a", choices=['u'],
-                        default='u', help='Action to perform')
-    parser.add_argument("-db_file",  type=str,
-                        help='File name of the database to update')
-    parser.add_argument("-n_proc", default=4, type=int,
-                        help='Number of processor to use')
+    parser.add_argument("-a", choices=['u', 'd'], default='u', help='Action to perform')
+    parser.add_argument("-db_file", type=str, help='File name of the database to update')
+    parser.add_argument("-n_proc", default=4, type=int, help='Number of processor to use')
     args = parser.parse_args()
     return args
