742bb0339522f77e958df6300f2bdcb7e45f4cbd
Daniel Duque <d.duque25@gmail.com>
2020-10-11 01:31:05 -0400

    Rebalancing change.

--- source/resources.py
+++ source/resources.py
@@ -8,13 +8,25 @@ import pandas as pd
 import numpy as np
 import copy
 import pickle
+import types
 from pathlib import Path
 
 project_path = Path(__file__).parent.parent
 accounts_path = project_path / 'accounts/'
 
 
+def example(self, a):
+    self.a = a
+    print(self.__dict__)
+
+
+def register_example(p):
+    p.example = types.MethodType(example, p)
+
+
 class Portfolio:
+    '''
+    '''
     def __init__(self):
         self._data = {}
     
@@ -22,14 +34,19 @@ class Portfolio:
         if asset in self._data:
             return self._data[asset]["qty"]
         else:
-            return None
+            return 0
+    
+    @property
+    def assets(self):
+        if not hasattr(self, '_assets'):
+            self._assets = list(self._data.keys())
+        return self._assets
     
     @classmethod
     def create_from_vectors(cls, assets, qty):
         p = cls()
         for (a, q) in zip(assets, qty):
             p._data[a] = {"qty": q}
-        
         return p
     
     @classmethod
@@ -64,8 +81,8 @@ class Portfolio:
         return self.__str__()
 
 
-OPERATION_SELL = 'sell'
-OPERATION_BUY = 'buy'
+OPERATION_SELL = 'Sell'
+OPERATION_BUY = 'Buy'
 OPERATION_TYPES = [OPERATION_SELL, OPERATION_BUY]
 
 
@@ -75,6 +92,12 @@ class Order:
         self.qty = qty
         self.price = price
         self.operation_type = operation_type
+    
+    def __str__(self):
+        return f"{self.operation_type} {self.ticker} : {self.qty} : {self.price}"
+    
+    def __repr__(self):
+        return self.__str__()
 
 
 class Account:
@@ -85,6 +108,7 @@ class Account:
         self.portfolios = {opening_date: Portfolio.create_empty()}
         self.last_transaction = opening_date
         self.cash_onhand = 0
+        self.abc = -1
     
     def deposit(self, deposit_date, amount):
         self.cash_flow.append({
@@ -134,25 +158,6 @@ class Account:
                     ignore_index=True)
             return True
         return False
-    
-    def build_porfolio_snapshots(self, start=None, end=None, time_resolution="days"):
-        '''
-            Constructs the history of the account from the the start date given
-            as a paramter.
-            
-            Args:
-            start (datetime): start date from which the history will be computed
-                If None, the histoty is computed form the account's opning date.
-            end (datetime): end date from of the history. If None, the history is
-                compute until dt.today().
-            time_resolution (str): granularity of the history ("weeks", "days", "hours"),
-                default is "days"
-            
-            Returns:
-            history (dict of datetime-porfolios): a dictionary with all the porfolios that
-                the account has 
-        '''
-        pass
 
 
 def save_account(account):
@@ -165,29 +170,67 @@ def save_account(account):
         writer.write(backup_name)
     backup_file = path_to_account / backup_name
     pickle.dump(account, backup_file.open("wb"), pickle.HIGHEST_PROTOCOL)
+    
+    # Save classes for future migration
+    path_portfolio_class = path_to_account / 'PortfolioClass'
+    pickle.dump(Portfolio, path_portfolio_class.open("wb"), pickle.HIGHEST_PROTOCOL)
+    path_account_class = path_to_account / 'AccoundClass'
+    pickle.dump(Account, path_account_class.open("wb"), pickle.HIGHEST_PROTOCOL)
 
 
 def load_account(account_name):
     path_to_account = accounts_path / account_name
+    path_portfolio_class = path_to_account / 'PortfolioClass'
+    path_account_class = path_to_account / 'AccoundClass'
     if not path_to_account.exists():
         return None
+    '''
+    Portfolio = None
+    Account = None
+    if path_portfolio_class.exists() and path_account_class.exists():
+        Portfolio = pickle.load(path_portfolio_class.open("rb"))
+        Account = pickle.load(path_account_class.open("rb"))
+    '''
     acc_file = None
     with open(path_to_account / "index.txt") as index_file:
         hist = index_file.read()
         hist_list = hist.split(".acc")
         hist_list.reverse()
         for acc_backup in hist_list:
-            backup_name = acc_backup + ".acc"
-            try:
-                backup_file = path_to_account / backup_name
-                account = pickle.load(backup_file.open("rb"))
-                return account
-            except Exception as identifier:
-                print(identifier)
-                print("WARNING: account backup {backup_name} not loaded.")
+            if len(acc_backup) > 0:
+                backup_name = acc_backup + ".acc"
+                try:
+                    backup_file = path_to_account / backup_name
+                    account = pickle.load(backup_file.open("rb"))
+                    print(f"INFO: account backup {backup_name} loaded succesfuly.")
+                    return account
+                except Exception as identifier:
+                    print(identifier)
+                    print(f"WARNING: account backup {backup_name} not loaded.")
+
+
+def generate_orders(old_portfolio, new_portfolio, prices):
+    '''
+        Generates a list of orders given two porfolios and the prices used to optimize
+        the new portfolio. Note that buy/sell prices for these orders might be (slightly) 
+        different as since the orders are placed manually after the fact.
+    '''
+    orders = []
+    assets = set()
+    assets.update(old_portfolio.assets)
+    assets.update(new_portfolio.assets)
+    for asset in assets:
+        old_position = old_portfolio.get_position(asset)
+        new_position = new_portfolio.get_position(asset)
+        if old_position < new_position:  # Buy
+            orders.append(Order(asset, new_position - old_position, prices[asset], OPERATION_BUY))
+        elif old_position > new_position:  # Sell
+            orders.append(Order(asset, old_position - new_position, prices[asset], OPERATION_SELL))
+    return orders
 
 
 if __name__ == '__main__':
+    '''
     dd_account = Account("Daniel Duque", dt.datetime(2020, 9, 14, 9, 30))
     dd_account.deposit(dt.datetime(2020, 9, 14, 9, 30), 1000)
     dd_account.update_account(dt.datetime(2020, 9, 14, 9, 35), [
@@ -197,11 +240,28 @@ if __name__ == '__main__':
         Order('MRO', 2, 4.47, OPERATION_BUY),
         Order('WMT', 2, 136.14, OPERATION_BUY),
         Order('CTXS', 1, 134.02, OPERATION_BUY),
-        Order('VS', 1, 60.02, OPERATION_BUY),
+        Order('VZ', 1, 60.02, OPERATION_BUY),
         Order('KR', 3, 33.68, OPERATION_BUY),
         Order('HLT', 1, 88.18, OPERATION_BUY),
         Order('SJM', 1, 113.49, OPERATION_BUY),
     ])
-    print(dd_account.cash_onhand)
-    print(dd_account.portfolios)
+    save_account(dd_account)
+    del dd_account
+    '''
+    dd_account = load_account("Daniel Duque")
+    dd_account.update_account(dt.datetime(2020, 9, 30, 9, 1), [
+        Order('AAPL', 3, 222.95, OPERATION_BUY),
+    ])
+    #print("begin registration")
+    #register_example(dd_account)
+    #dd_account.example(12312321)
+    #dd_account.hola = "hola"
+    save_account(dd_account)
+    del dd_account
+    dd_account = load_account("Daniel Duque")
+    
+    for p in dd_account.portfolios:
+        print(p.b)
+    print(dd_account.__dict__)
+    
     ##save_account(dd_account)
---
+++ source/test rebalance.py
@@ -0,0 +1,113 @@
+"""
+Created on Thu May 23 22:36:26 2019
+@author: dduque
+"""
+'''
+Setup paths
+'''
+import sys
+import os
+print(sys.version)
+import datetime as dt
+import pandas as pd
+import numpy as np
+import backtest as bt
+import database_handler as dbh
+from opt_tools import cvar_model_pulp, cvar_model_ortools
+from resources import Portfolio, Account, load_account, generate_orders
+
+path_to_file = os.path.dirname(os.path.realpath(__file__))
+parent_path = os.path.abspath(os.path.join(path_to_file, os.pardir))
+sys.path.append(parent_path)
+file_name = 'close.pkl'
+start_date = dt.datetime(2020, 1, 20)  #Initial date for the training period
+end_date_train = dt.datetime(2020, 9, 25)  #Final date for the training period
+end_date_test = dt.datetime(2021, 9, 4)  #Final date for the backtesting period
+outlier_return = 10  #Return threshold to eliminate outliters; a 10 means 1,000% return on a single day
+ini_capital = 1_000  #Initial capital available to be invested
+
+sp500 = dbh.yf.Ticker("^GSPC")  # Ticker
+sp500history = sp500.history(period='max', interval='1d')['Close']
+sp500history = sp500history[sp500history.index >= start_date]
+sp500_stocks = dbh.get_sp500_tickers()
+sp500_stocks_tickers = list(sp500_stocks.keys())
+db_all, _ = dbh.get_returns(data_file=file_name,
+                            start_date=start_date,
+                            stocks=sp500_stocks_tickers,
+                            outlier_return=outlier_return)
+
+# Train set
+stock_universe = list(db_all.columns)
+db, db_r = dbh.get_returns(data_file=file_name, start_date=start_date, end_date=end_date_train, stocks=stock_universe)
+# ['ABC','MSFT', 'AMZN', 'GOOGL', 'GE', 'F', 'MMM', 'ATVI'])
+data = np.array(db_r)
+'''
+Create model with default parameters
+'''
+price = db.iloc[-1]  # Last row is the current price
+print('Buy date: ', db.index.values[-1])
+n_stocks = len(db_r.columns)
+sp500_benchmark = pd.Series(data=np.ones(n_stocks) / n_stocks, index=db_r.columns)
+# opt_model = cvar_model_pulp(data, price, budget=ini_capital, fractional=False)
+dd_account = load_account("Daniel Duque")
+dd_portfolio = dd_account.portfolios[dd_account.last_transaction]
+print(dd_portfolio)
+opt_model = cvar_model_ortools(data,
+                               price,
+                               current_portfolio=dd_portfolio,
+                               budget=ini_capital,
+                               fractional=False,
+                               portfolio_delta=5)
+'''
+Solve parametricly in \beta
+'''
+portfolios = []
+portfolio_stats = []
+portfolio_names = []
+for cvar_beta in [0.8]:  # [i / 10 for i in range(1)]:
+    cvar_sol1, cvar_stats1 = opt_model.change_cvar_params(cvar_beta=cvar_beta)
+    portfolios.append(cvar_sol1[cvar_sol1.qty > 0])
+    portfolio_stats.append(cvar_stats1)
+    portfolio_names.append(f'cvar_{cvar_beta}')
+print(generate_orders(dd_portfolio, Portfolio.create_from_vectors(portfolios[-1].index, portfolios[-1].qty), price))
+# SP500 porfolio
+sp500_value = sum(sp500_stocks[s]['market_cap'] for s in db_r.columns)
+allocation = np.array([sp500_stocks[s]['market_cap'] for s in db_r.columns]) / sp500_value
+sp500_portfolio = pd.DataFrame({
+    'price': price,
+    'qty': allocation * ini_capital / price,
+    'position': allocation * ini_capital,
+    'allocation': allocation,
+    'side': 'buy'
+})
+portfolios.append(sp500_portfolio)
+stats_sp_500 = {'mean': opt_model.r_bar.dot(allocation), 'std': np.sqrt(allocation.dot(opt_model.cov.dot(allocation)))}
+portfolio_stats.append(stats_sp_500)
+portfolio_names.append('SP500')
+
+for (p, ps) in zip(portfolios, portfolio_stats):
+    p2 = p.copy()
+    p2['name'] = [sp500_stocks[s]['name'] for s in p.index]
+    p2['sector'] = [sp500_stocks[s]['sector'] for s in p.index]
+    p2['subsector'] = [sp500_stocks[s]['subsector'] for s in p.index]
+    print(p2, ps)
+print(portfolio_names)
+import pickle
+out_portfolios = portfolios, portfolio_stats, portfolio_names
+pickle.dump(out_portfolios, open('./cvar_portfolio.pkl', 'wb'), pickle.HIGHEST_PROTOCOL)
+
+if False:  # Run a stored portfolio
+    file = open("cvar_portfolio_DD.pkl", 'rb')
+    portfolios, portfolio_stats = pickle.load(file)
+# Back test
+start_date_test = end_date_train
+db = db_all[(db_all.index >= start_date_test) & (db_all.index <= end_date_test)]
+portfolio_paths = []
+for p in portfolios[:]:
+    portfolio_paths.append(bt.run_backtest(p, db, start_date_test, end_date_test, 20))
+
+sp500history = np.array(sp500history[(sp500history.index >= start_date_test) & (sp500history.index <= end_date_test)])
+factor = ini_capital / sp500history[0]
+sp500history = factor * sp500history
+# portfolio_paths.append([sp500history])
+bt.plot_backtests(portfolio_paths, portfolio_names)
